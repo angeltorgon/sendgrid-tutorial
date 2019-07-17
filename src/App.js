6c349808f4579d48500b81cc2a576167e72d8ec1
angel-torres <alas4224@gmail.com>
2019-07-16 22:51:22 -0500

    create controlled inputs and bind them to useState

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [ recipient, setRecipient ] = useState('');
+  const [ sender, setSender ] = useState('');
+  const [ subject, setSubject ] = useState('');
+  const [ text, setText ] = useState('');
+
   return (
     <div className="App">
       <form>
-        <input type="text" onChange={changeHandler()} name="" value="" />
-        <input type="text" onChange={changeHandler()} name="" value="" />
-        <input type="text" onChange={changeHandler()} name="" value="" />
-        <input type="text" onChange={changeHandler()} name="" value="" />
+        <input type="text" onChange={setRecipient(e.target.value)} name="recipient" value={recipient} />
+        <input type="text" onChange={setSender(e.target.value)} name="sender" value={sender} />
+        <input type="text" onChange={setSubject(e.target.value)} name="subject" value={subject} />
+        <input type="text" onChange={setText(e.target.value)} name="text" value={text} />
       </form>
     </div>
   );
